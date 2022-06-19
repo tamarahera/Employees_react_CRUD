5a407c391075b278ad8048a227d00be03f03905c
Tamara <tamara.herashchenko@gmail.com>
2022-06-19 15:41:46 +0200

    Update onToggleProp

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,7 +1,7 @@
 import './employees-list-item.css';
 
 const EmployeesListItem = (props) => {
-    const {name, salary, onDelete, onToggleIncrease, onToogleRise, increase, rise} = props;
+    const {name, salary, onDelete, onToggleProp, increase, rise} = props;
 
     let classNames = "list-group-item d-flex justify-content-between";
     if (increase) {
@@ -15,12 +15,12 @@ const EmployeesListItem = (props) => {
 
     return (
         <li className={classNames}>
-            <span onClick={onToogleRise} data-toggle="rise" className={classLike}>{name}</span>
+            <span onClick={onToggleProp} data-toggle="rise" className={classLike}>{name}</span>
             <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
             <div className='d-flex justify-content-center align-items-center'>
                 <button type="button"
                         className="btn-cookie btn-sm"
-                        onClick={onToggleIncrease}
+                        onClick={onToggleProp}
                         data-toggle="increase">
                     <i className="fas fa-cookie"></i>
                 </button>
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,7 +2,7 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 
 import './employees-list.css';
 
-const EmployeesList = ({data, onDelete, onToggleIncrease, onToogleRise}) => {
+const EmployeesList = ({data, onDelete, onToggleProp}) => {
 
     const elements = data.map(item => {
         const {id, ...itemProps} = item;
@@ -11,8 +11,7 @@ const EmployeesList = ({data, onDelete, onToggleIncrease, onToogleRise}) => {
                 key={id} 
                 {...itemProps}
                 onDelete={() => onDelete(id)}
-                onToggleIncrease={() => onToggleIncrease}
-                onToogleRise={() => onToogleRise}
+                onToggleProp={(e) => onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))}
             />
         )
     });
