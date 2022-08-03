74ed5fee2aa4fa189c3c77febd9dac0fa0436ac9
tamara_hera <tamara.herashchenko@gmail.com>
2022-08-03 17:25:06 +0200

    Add app-filter

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,23 +1,29 @@
 import './app-filter.css';
 
-const AppFilter = () => {
+const AppFilter = (props) => {
+    const buttonsData = [
+        {name: 'all', label: 'All employees'},
+        {name: 'rise', label: 'For promotion'},
+        {name: 'moreThen1000', label: 'Salary over $1000'}
+    ]
+
+    const buttons = buttonsData.map(({name, label}) => {
+        const active = props.filter === name; // true in active
+        const clazz = active ? 'btn-light' : 'btn-outline-light'
+        return (
+            <button     
+                className={`btn ${clazz}`}
+                type="button"
+                key={name}
+                onClick={() => props.onFilterSelect(name)}>
+                    {label}
+            </button>
+        )
+    })
+
     return (
         <div className="btn-group">
-            <button     
-                className="btn btn-light"
-                type="button">
-                    All employees
-            </button>
-            <button     
-                className="btn btn-outline-light"
-                type="button">
-                    For promotion
-            </button>
-            <button     
-                className="btn btn-outline-light"
-                type="button">
-                    Salary over $1000
-            </button>
+            {buttons}
         </div>
     );
 }
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -15,8 +15,10 @@ class App extends Component {
             data: [
                 {name: 'Alex S.', salary: 950, increase: true, rise: false, id:"1"},
                 {name: 'Jack K.', salary: 3000, increase: false, rise: true, id:"2"},
-                {name: 'George O.', salary: 1980, increase: true, rise: false, id:"3"},
-            ]
+                {name: 'George O.', salary: 1980, increase: true, rise: false, id:"3"}
+            ],
+            term: '', 
+            filter: 'all'
         }
         this.maxId = 4
     }
@@ -60,7 +62,6 @@ class App extends Component {
                 return item
             })
         }))
-
     }
 /*     onToggleIncrese = (id) => {
         this.setState(({data}) => {
@@ -87,23 +88,52 @@ class App extends Component {
         }))
     } */
 
-    
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1;
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term});
+    }
+
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item => item.rise);
+            case 'moreThen1000':
+                return items.filter(item => item.salary > 1000);
+            default:
+                return items;
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({filter});
+    }
     
 
     render() {
+        const {data, term, filter} = this.state;
         const employees = this.state.data.length;
         const awarded = this.state.data.filter(item => item.increase).length;
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter);
         return (
             <div className="app">
                 <AppInfo employees={employees}
                          awarded={awarded} />
     
                 <div className="search-panel">
-                    <SearchPanel/>
-                    <AppFilter/>
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
                 </div>
     
-                <EmployeesList data={this.state.data}
+                <EmployeesList data={visibleData}
                                onDelete={this.deleteItem}
                                onToggleProp={this.onToggleProp}/>
                 <EmployeesAddForm onAdd={this.addItem}/>
