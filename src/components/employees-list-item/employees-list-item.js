27eba0844585d943ea2d81a2a9700cfa05591e64
tamara_hera <tamara.herashchenko@gmail.com>
2022-10-13 10:39:11 +0200

    Update employees-list-item.js

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,4 +1,4 @@
-import './employees-list-item.css';
+import './employees-list-item.scss';
 
 const EmployeesListItem = (props) => {
     const {name, salary, onDelete, onToggleProp, increase, rise, onChangeSalary} = props;
